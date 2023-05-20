1e9f23a1f3f454bd792be5466ad83db53c92dd26
Joaquin <joaquinacholopez@gmail.com>
2023-05-20 12:03:07 -0300

    controller de session

---
+++ src/controllers/sessionController.js
@@ -0,0 +1,82 @@
+const { userModel } = require('../dao/models/usersModel')
+const { createHashedPass } = require('../utils/bcryptPass')
+
+class sessionManagerController {
+    loginFunction = async ( req , res ) => {
+        try {
+            req.session.user = {
+                username: req.user.username,
+                email: req.user.email,
+                rol: req.user.rol
+            }
+            
+            res.redirect('/home')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    failedRegister = ( req , res ) => {
+        let result = {
+            status: "Error.",
+            message: "El usuario ya existe."
+        }
+        res.status(401).send(result)
+    }
+
+    failedLogin = ( req , res ) => {
+        let result = {
+            status: "Error.",
+            message: "El usuario no existe."
+        }
+        res.status(401).send(result)
+    }
+
+    githubCallback = async ( req , res ) => {
+        try {
+            req.session.user = {
+                username: req.user.username,
+                email: req.user.email,
+                rol: req.user.rol
+            }
+    
+            res.redirect('/home')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    recoveryPassword = async ( req , res ) => {
+        const {email , newPassword} = req.body
+    
+        const user = await userModel.findOne({email})
+        if (!user) return res.send({status: 'error', message: 'No hay ninguna cuenta registrada con ese correo.'})
+    
+        const hashedPassword = createHashedPass(newPassword)
+    
+        await userModel.updateOne({email},{password:hashedPassword})
+    
+        req.session.user = {
+            username: user.username,
+            email: user.email,
+            rol: user.rol
+        }
+        res.redirect('/home')
+    }
+
+    currentSession = async ( req , res ) => {
+        if (!req.session.user) {
+            res.redirect('/session/login')
+        }
+        res.send(req.session.user)
+    }
+
+    logoutSession = ( req , res ) => {
+        req.session.destroy (err => {
+            if(err) return res.status(400).send({status: 'Logout error', message: err});
+            res.redirect('/session/login')
+        })
+    }
+}
+
+module.exports = sessionManagerController
--- src/routes/routers/sessionRouter.js
+++ src/routes/routers/sessionRouter.js
@@ -1,9 +1,18 @@
 const {Router} = require('express')
-const { userModel } = require('../../dao/models/usersModel')
-const { createHashedPass } = require('../../utils/bcryptPass')
 const passport = require('passport')
+const sessionManagerController = require('../../controllers/sessionController')
 
 const sessionRouter = Router()
+const {
+    loginFunction,
+    failedRegister,
+    failedLogin,
+    githubCallback,
+    recoveryPassword,
+    currentSession,
+    logoutSession
+} = new sessionManagerController()
+
 
 sessionRouter.get('/register' , ( req , res ) => {
     res.render('register')
@@ -13,92 +22,30 @@ sessionRouter.post('/register' , passport.authenticate('register', {failureRedir
     res.redirect('/home')
 })
 
-sessionRouter.get('/failedregister' , ( req , res ) => {
-    let result = {
-        status: "Error.",
-        message: "El usuario ya existe."
-    }
-    res.status(401).send(result)
-})
 
 sessionRouter.get('/login' , ( req , res ) => {
     res.render('login')
 })
 
-sessionRouter.post('/login' , passport.authenticate('login', {failureRedirect:'/session/failedlogin'}), async ( req , res ) => {
-    try {
-        req.session.user = {
-            username: req.user.username,
-            email: req.user.email,
-            rol: req.user.rol
-        }
+sessionRouter.post('/login' , passport.authenticate('login', {failureRedirect:'/session/failedlogin'}), loginFunction)
 
-        res.redirect('/home')
-    } catch (error) {
-        console.log(error);
-    }
-})
+sessionRouter.get('/failedregister' , failedRegister)
 
-sessionRouter.get('/failedlogin' , ( req , res ) => {
-    let result = {
-        status: "Error.",
-        message: "El usuario no existe."
-    }
-    res.status(401).send(result)
-})
+sessionRouter.get('/failedlogin' , failedLogin)
 
 sessionRouter.get('/github' , passport.authenticate('github'))
 
-sessionRouter.get('/githubcallback' , passport.authenticate('github', {failureRedirect:'/session/failedlogin'}) , async ( req , res ) => {
-    try {
-        req.session.user = {
-            username: req.user.username,
-            email: req.user.email,
-            rol: req.user.rol
-        }
-
-        res.redirect('/home')
-    } catch (error) {
-        console.log(error);
-    }
-})
+sessionRouter.get('/githubcallback' , passport.authenticate('github', {failureRedirect:'/session/failedlogin'}) , githubCallback)
 
 sessionRouter.get('/recovery-password' , ( req , res ) => {
     res.render('new_login')
 })
 
-sessionRouter.post('/recovery-password' , async ( req , res ) => {
-    const {email , newPassword} = req.body
+sessionRouter.post('/recovery-password' , recoveryPassword)
 
-    const user = await userModel.findOne({email})
-    if (!user) return res.send({status: 'error', message: 'No hay ninguna cuenta registrada con ese correo.'})
+sessionRouter.get('/current' , currentSession)
 
-    const hashedPassword = createHashedPass(newPassword)
-
-    let result = await userModel.updateOne({email},{password:hashedPassword})
-
-    req.session.user = {
-        username: user.username,
-        email: user.email,
-        rol: user.rol
-    }
-    res.redirect('/home')
-})
-
-sessionRouter.get('/current' , async ( req , res ) => {
-    if (!req.session.user) {
-        res.redirect('/session/login')
-    }
-    res.send(req.session.user)
-})
-
-sessionRouter.get('/logout', ( req , res ) => {
-    req.session.destroy (err => {
-        if(err) return res.status(400).send({status: 'Logout error', message: err});
-        res.redirect('/session/login')
-    })
-})
-
+sessionRouter.get('/logout', logoutSession)
 
 
 module.exports = {
